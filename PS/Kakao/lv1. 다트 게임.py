9d1b53fb648e07c5754cf8384860b5857cb7c0bc
unhhyyeexx <unhhyyee66@gmail.com>
2022-03-01 21:19:06 +0900

    0301

---
+++ PS/Kakao/lv1. 다트 게임.py
@@ -0,0 +1,29 @@
+def solution(dartResult):
+    answer = 0
+    score = []
+    origin = ''
+    sdt = {'S':1, 'D':2, 'T':3}
+    for d in dartResult:
+        if d in list(sdt.keys()):
+            score.append(int(origin)**sdt[d])
+            origin = ''
+        elif d == '*':
+            tmp = []
+            i = 0
+            while score and i<2:
+                tmp.append(score.pop() * 2)
+                i+= 1
+            score += reversed(tmp)
+        elif d == '#':
+            tmp = score.pop()
+            score.append(tmp * (-1))
+        else:
+            origin += d
+
+    answer = sum(score)
+
+    return answer
+
+neo = '1S2D*3T*'
+frodo = solution(neo)
+print(frodo)
