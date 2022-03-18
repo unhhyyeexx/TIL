35815279f14a260da792bac604db84cd549d3151
unhhyyeexx <unhhyyee66@gmail.com>
2022-03-18 19:32:06 +0900

    0318

---
+++ PS/SWEA/4012 요리사.py
@@ -0,0 +1,19 @@
+
+answer = 1e9
+
+def dfs(cnt, k):
+    global answer
+    if cnt == N//2:
+        a, b = [], []
+        for i in range(N):
+
+
+    return
+
+T = int(input())
+for t in range(T):
+    N = int(input())
+    lst = [list(map(int, input().split())) for _ in range(N)]
+    visit = [0]*(N+1)
+    dfs(0,0)
+
