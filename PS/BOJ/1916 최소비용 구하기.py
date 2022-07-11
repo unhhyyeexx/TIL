c4c142d6cad1bcc5d4aab718b0e20fe9227e7941
unhhyyeexx <unhhyyee66@gmail.com>
2022-07-11 23:26:57 +0900

    0711

---
+++ PS/BOJ/1916 최소비용 구하기.py
@@ -0,0 +1,33 @@
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+graph = [[] for i in range(n+1)]
+distance = [INF] * (n+1)
+
+for _ in range(m):
+    s, g, c = map(int, input().split())
+    graph[s].append((g, c))
+
+start, end = map(int, input().split())
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+print(distance[end])
