355ffcb1f56e5a45ccd04b80456aba516b3b8491
unhhyyeexx <unhhyyee66@gmail.com>
2022-07-18 23:18:28 +0900

    0718

---
+++ PS/BOJ/2174 로봇 시뮬레이션.py
@@ -0,0 +1,13 @@
+
+import sys
+input = sys.stdin.readline
+
+def solution():
+
+    return
+
+
+a, b = map(int, input().split())
+n, m = map(int, input().split())
+first = [list(map(int, input().split())) for _ in range(n)]
+order = [list(map(int, input().split())) for _ in range(m)]
